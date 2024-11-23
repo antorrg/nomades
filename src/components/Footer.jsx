3d5be8a705645e1f382004966e8bf502f19bca77
antorrg <114789901+antorrg@users.noreply.github.com>
2024-11-23 08:12:41 -0300

    Ajuste commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Añade un desplazamiento suave
+    });
+  };
   return (
     <div className='text-muted py-5'>
     <div className='container'>
     <hr></hr>
      <p className='float-end mb-1'>
-        <a className='btn.btn-primary' href='/'>Volver</a>
+        <a className='btn btn-sm btn-outline-secondary' href='#' onClick={scrollToTop}style={{cursor:'pointer'}}>Volver</a>
      </p>
      <p className='mb-1'>Este es el footer</p>
     </div>
@@ -15,12 +21,3 @@ const Footer = () => {
 }
 
 export default Footer
-
-// footer.text-muted.py-5
-// .container
-//   p.float-end.mb-1
-//     a.btn.btn-primary(href="/") Volver
-//   p.mb-1 Esta es una copia de boostrap pronto le vamos a cambiar el nombre.
-//   p.mb-0
-//     | New to Bootstrap?
-//     a(href="/") Visit the homepage
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,10 +13,11 @@ const Header = () => {
             Cabañas de pastores
         </h3>
         <nav className='nav nav-masthead justify-content-center float-md-end caption-nav'>
-            <a className='nav-link fw-bold py-1 px-0 active' aria-current='page' href='/'>Home</a>
+            <Link className='nav-link fw-bold py-1 px-0 active' to='/'>Inicio</Link>
             {authenticated?
             <button className='nav-link fw-bold py-1 px-0 active' onClick={()=>{navigate('/admin')}}>Admin</button> : null
             }
+            <Link className='nav-link fw-bold py-1 px-0 active' to='/' >Videos</Link>
             <Link className='nav-link fw-bold py-1 px-0 active' to='/contacto' >Contacto</Link>
             <Link className='nav-link fw-bold py-1 px-0 active' to='/acerca'>Acerca de </Link>
         </nav>
